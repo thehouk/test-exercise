b8a378ba7082907c1dd8fa462ed2354a9e1f201a
Israel Shenkar <israel@stox.com>
2018-12-13 12:49:53 +0200

    added hellow to counter

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 const counter = require('./counter')
-app.get('*', (req, res) => res.send(counter.getCounter()))
+app.get('*', (req, res) => res.send("Hellow Israel"+counter.getCounter()))
 app.post('*', (req, res) => res.send(counter.increment()))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
